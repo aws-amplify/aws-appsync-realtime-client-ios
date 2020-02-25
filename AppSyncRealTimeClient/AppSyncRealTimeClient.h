422740f9cb07965c36b5929838958a870101ee6e
Michael law <1365977+lawmicha@users.noreply.github.com>
2020-02-25 15:20:48 -0800

    Adding xcode project

---
+++ AppSyncRealTimeClient/AppSyncRealTimeClient.h
@@ -0,0 +1,19 @@
+//
+//  AppSyncRealTimeClient.h
+//  AppSyncRealTimeClient
+//
+//  Created by Law, Michael on 2/25/20.
+//  Copyright © 2020 amazonaws. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for AppSyncRealTimeClient.
+FOUNDATION_EXPORT double AppSyncRealTimeClientVersionNumber;
+
+//! Project version string for AppSyncRealTimeClient.
+FOUNDATION_EXPORT const unsigned char AppSyncRealTimeClientVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <AppSyncRealTimeClient/PublicHeader.h>
+
+
