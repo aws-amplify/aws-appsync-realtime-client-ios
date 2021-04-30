35eccb323609270b4e9f1af7681a196129a8b6fc
Michael <1365977+lawmicha@users.noreply.github.com>
2021-04-30 08:35:50 -0700

    add more testing, and add changelog

--- AppSyncRealTimeClientIntegrationTests/StarscreamAdapterTests.swift
+++ AppSyncRealTimeClientIntegrationTests/StarscreamAdapterTests.swift
@@ -9,13 +9,15 @@ import XCTest
 @testable import AppSyncRealTimeClient
 import Starscream
 
-class StarscreamAdapterTests: AppSyncRealTimeClientIntegrationTests {
+class StarscreamAdapterTests: AppSyncRealTimeClientTestBase {
 
     func testConnectDisconnect() throws {
         let starscreamAdapter = StarscreamAdapter()
         let apiKeyAuthInterceptor = APIKeyAuthInterceptor(apiKey)
         let request = AppSyncConnectionRequest(url: url)
         let signedRequest = apiKeyAuthInterceptor.interceptConnection(request, for: url)
+        let expectedPerforms = expectation(description: "total performs")
+        expectedPerforms.expectedFulfillmentCount = 1_000
         DispatchQueue.concurrentPerform(iterations: 1_000) { _ in
             starscreamAdapter.connect(
                 url: signedRequest.url,
@@ -23,7 +25,9 @@ class StarscreamAdapterTests: AppSyncRealTimeClientIntegrationTests {
                 delegate: nil
             )
             starscreamAdapter.disconnect()
+            expectedPerforms.fulfill()
         }
+        wait(for: [expectedPerforms], timeout: 1)
         XCTAssertFalse(starscreamAdapter.isConnected)
     }
 }
