6d44d1c3ce55d59d5ac0d99f45e057af3707d62f
Chris Ameter <29186822+ameter@users.noreply.github.com>
2022-04-26 19:56:43 -0600

    cleaned up commented out code

--- AppSyncRealTimeClient/ConnectionProvider/AppsyncRealtimeConnection/RealtimeConnectionProvider+MessageInterceptableAsync.swift
+++ AppSyncRealTimeClient/ConnectionProvider/AppsyncRealtimeConnection/RealtimeConnectionProvider+MessageInterceptableAsync.swift
@@ -24,13 +24,4 @@ extension RealtimeConnectionProvider: MessageInterceptableAsync {
         
         return finalMessage
     }
-    
-//    public func addInterceptor(_ interceptor: MessageInterceptor) {
-//        messageInterceptors.append(interceptor)
-//    }
-//
-//    public func interceptMessage(_ message: AppSyncMessage, for endpoint: URL) -> AppSyncMessage {
-//        let finalMessage = messageInterceptors.reduce(message) { $1.interceptMessage($0, for: endpoint) }
-//        return finalMessage
-//    }
 }
