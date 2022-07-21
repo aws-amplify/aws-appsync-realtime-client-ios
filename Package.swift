586745dc42a442a925f33d6d9ac160270fe80755
Brennan Stehling <stehlib@amazon.com>
2022-07-21 12:37:20 -0400

    defines platform versions in Package.swift #97

--- Package.swift
+++ Package.swift
@@ -1,10 +1,14 @@
-// swift-tools-version:5.3
+// swift-tools-version:5.5
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
 
 let package = Package(
     name: "AppSyncRealTimeClient",
+    platforms: [
+        .iOS(.v13),
+        .macOS(.v12)
+    ],
     products: [
         .library(
             name: "AppSyncRealTimeClient",
