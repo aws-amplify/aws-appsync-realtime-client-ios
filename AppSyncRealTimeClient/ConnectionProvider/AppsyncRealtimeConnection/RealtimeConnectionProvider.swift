8dea675729b84e3c6e9994a0bb79ee019cf72934
Brennan Stehling <stehlib@amazon.com>
2022-07-25 10:00:13 -0700

    sets OS versions for os_log

--- AppSyncRealTimeClient/ConnectionProvider/AppsyncRealtimeConnection/RealtimeConnectionProvider.swift
+++ AppSyncRealTimeClient/ConnectionProvider/AppsyncRealtimeConnection/RealtimeConnectionProvider.swift
@@ -87,7 +87,7 @@ public class RealtimeConnectionProvider: ConnectionProvider {
 
         connectivityMonitor.start(onUpdates: handleConnectivityUpdates(connectivity:))
 
-        if #available(iOS 13.0, *) {
+        if #available(iOS 13.0, macOS 10.15, tvOS 13.0, watchOS 6.0, *) {
             subscribeToLimitExceededThrottle()
         }
     }
