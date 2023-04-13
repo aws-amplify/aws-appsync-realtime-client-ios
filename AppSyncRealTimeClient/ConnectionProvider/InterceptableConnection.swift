915db7a08ad8857d3d20e94b34fac63efde88482
Jithin Roy <51138777+royjit@users.noreply.github.com>
2023-04-13 13:05:49 -0700

    feat: Add asynchronous version of interceptor protocol

--- AppSyncRealTimeClient/ConnectionProvider/InterceptableConnection.swift
+++ AppSyncRealTimeClient/ConnectionProvider/InterceptableConnection.swift
@@ -9,30 +9,52 @@ import Foundation
 
 /// Intercepts the connect request
 public protocol ConnectionInterceptable {
-
+    
     /// Add a new interceptor to the object.
     ///
     /// - Parameter interceptor: interceptor to be added
     func addInterceptor(_ interceptor: ConnectionInterceptor)
 
-    func interceptConnection(_ request: AppSyncConnectionRequest, for endpoint: URL) -> AppSyncConnectionRequest
+    @available(*, deprecated, message: "Use the async version under ConnectionInterceptableAsync or completion handler flavor")
+    func interceptConnection(_ request: AppSyncConnectionRequest,
+                             for endpoint: URL) -> AppSyncConnectionRequest
+    
+    func interceptConnection(_ request: AppSyncConnectionRequest,
+                             for endpoint: URL,
+                             completion: (AppSyncConnectionRequest) -> Void)
 }
 
 public protocol MessageInterceptable {
-
+    
     func addInterceptor(_ interceptor: MessageInterceptor)
 
+    @available(*, deprecated, message: "Use the async version under MessageInterceptableAsync or completion handler flavor")
     func interceptMessage(_ message: AppSyncMessage, for endpoint: URL) -> AppSyncMessage
+    
+    func interceptMessage(_ message: AppSyncMessage,
+                          for endpoint: URL,
+                          completion: (AppSyncMessage) -> Void)
 }
 
 public protocol ConnectionInterceptor {
 
-    func interceptConnection(_ request: AppSyncConnectionRequest, for endpoint: URL) -> AppSyncConnectionRequest
+    @available(*, deprecated, message: "Use the async version under ConnectionInterceptorAsync or completion handler flavor")
+    func interceptConnection(_ request: AppSyncConnectionRequest,
+                             for endpoint: URL) -> AppSyncConnectionRequest
+    
+    func interceptConnection(_ request: AppSyncConnectionRequest,
+                             for endpoint: URL,
+                             completion: (AppSyncConnectionRequest) -> Void)
 }
 
 public protocol MessageInterceptor {
 
+    @available(*, deprecated, message: "Use the async version under MessageInterceptorAsync or completion handler flavor")
     func interceptMessage(_ message: AppSyncMessage, for endpoint: URL) -> AppSyncMessage
+    
+    func interceptMessage(_ message: AppSyncMessage,
+                          for endpoint: URL,
+                          completion: (AppSyncMessage) -> Void)
 }
 
 public protocol AuthInterceptor: MessageInterceptor, ConnectionInterceptor {}
