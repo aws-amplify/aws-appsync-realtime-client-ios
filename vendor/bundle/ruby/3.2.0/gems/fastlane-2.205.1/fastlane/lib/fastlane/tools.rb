4dcc96e80a333afcca5e1949b4729804d96df762
aws-amplify-ops <aws-amplify-ops@amazon.com>
2023-12-04 20:59:52 +0000

    chore: release commit for 3.1.2

---
+++ vendor/bundle/ruby/3.2.0/gems/fastlane-2.205.1/fastlane/lib/fastlane/tools.rb
@@ -0,0 +1,34 @@
+module Fastlane
+  TOOLS = [
+    :fastlane,
+    :pilot,
+    :spaceship,
+    :produce,
+    :deliver,
+    :frameit,
+    :pem,
+    :snapshot,
+    :screengrab,
+    :supply,
+    :cert,
+    :sigh,
+    :match,
+    :scan,
+    :gym,
+    :precheck,
+    :trainer
+  ]
+
+  # a list of all the config files we currently expect
+  TOOL_CONFIG_FILES = [
+    "Appfile",
+    "Deliverfile",
+    "Fastfile",
+    "Gymfile",
+    "Matchfile",
+    "Precheckfile",
+    "Scanfile",
+    "Screengrabfile",
+    "Snapshotfile"
+  ]
+end
